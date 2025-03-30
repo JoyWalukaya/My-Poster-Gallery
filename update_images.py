c19003fa90386ca78d1e7dc5844cde67bddbf68f
JoyWalukaya <walukayajoy@gmail.com>
2025-03-30 21:53:28 +0300

    poster update

--- update_images.py
+++ update_images.py
@@ -1,7 +1,8 @@
 import os
 import json
 
-image_folder = "images"
+# Change this to point to the current directory (where your images are located)
+image_folder = "."  # Refers to the current folder (MyPosters'Gallery)
 images = [f for f in os.listdir(image_folder) if f.endswith(("png", "jpg", "jpeg"))]
 
 with open("images.json", "w") as file:
